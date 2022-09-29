475bd3b8c96cd0d7d6ca2bb0fad4c4c323f74027
rai-shahnawaz <rai.shahnawaz@emblemtechnologies.com>
2022-09-29 18:13:18 +0500

    url_patterns templates

--- core/urls.py
+++ core/urls.py
@@ -13,17 +13,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+from core.settings import BASE_DIR
 from django.contrib import admin
 from django.urls import path, include
-
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('snippets.urls')),
-    path('index/', TemplateView.as_view(template_name='index.html'),
-                        name='index'),
-    path('index-2/', TemplateView.as_view(template_name='index-2.html'),
-                        name='index-2'),
+    
+    # path('index/', TemplateView.as_view(template_name='index.html'),
+    #                     name='index'),
+    # path('index-2/', TemplateView.as_view(template_name='index-2.html'),
+    #                     name='index-2'),
 ]
+
+dir = os.path.join(BASE_DIR, 'templates')
+file_list = os.listdir(dir)
+for file in file_list:
+    path_url = file.replace('.html', '/')
+    name = file.replace('.html', '')
+    route = path(path_url, TemplateView.as_view(template_name=file),
+                        name=name)
+    urlpatterns.append(route)
